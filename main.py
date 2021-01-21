94a1413ef6c1a10c9abc87299eefcbebc0460b5a
fabrizziorivero <Manzana22#>
2021-01-20 20:17:00 -0600

    Adding relational matrix function proposal

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-def generate_rowEchelon(matrix):
-    return matrix.rref()
+def generate_subspace(vectors):
+    matx = matrix(vectors)
+    echelon_mat = matx.rref()
+    rows, columns = echelon_mat.dimensions()
+    zero_rows = []
+    for row in range(0, rows):
+        zero_counter = 0
+        for column in range(0, columns):
+            if echelon_mat[row][column] == 0:
+                zero_counter = zero_counter +1
+        if zero_counter == columns:
+            zero_rows.append(row)
+            
+    echelon_mat = echelon_mat.delete_rows(zero_rows, check= True)
+    print(echelon_mat)
 
-def variablesDefinition(A, b):
+
+            
+    
+def solve_equations(A, b):
     m,n=A.dimensions()
     x,y=b.dimensions()
     if(m == x and y == 1):
@@ -21,9 +37,6 @@ def variablesDefinition(A, b):
                         if C[row][right_value]!=0 and right_value not in columnas_libres:
                             columnas_libres.append(right_value)
                     break
-                            
-                    
-                    
                 print(f'libres:{columnas_libres}')       
 
         print("***** Soluciones *****")
@@ -31,10 +44,11 @@ def variablesDefinition(A, b):
             print(expresiones[e])
 
 #Para que el usuario defina la matriz y el vector 
-A=matrix([[1,2,-1,3],[3,5,-4,7]])
-b=matrix([[0],[0]])
-
-variablesDefinition(A, b)
+#A=matrix([[1,2,-1,3],[3,5,-4,7]])
+#b=matrix([[0],[0]])
+vectors = [(1, 2, 3, 4), (2,3, 1, 4), (2, 3,1, 4), (2, 3, 1 , 4)]
+generate_subspace(vectors)
+#variablesDefinition(A, b)
 
 
 
